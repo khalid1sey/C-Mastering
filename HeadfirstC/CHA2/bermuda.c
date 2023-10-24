08a5a9aeda1f3af7f4948dee37caa7faad3e9904
khalid1sey <lilahseyfu@gmail.com>
2023-10-24 20:10:18 +0300

    Done Task 2

--- HeadfirstC/CHA2/bermuda.c
+++ HeadfirstC/CHA2/bermuda.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void go_south_east(int lat, int lon)
+void go_south_east(int *lat, int *lon)
 {
-    lat = lon - 1;
-    lon = lon + 1;
+    *lat = *lat - 1;
+    *lon = *lon + 1;
 }
 
 int main(void)
 {
-    int latitude = 32;
+    int latitude = 35;
     int longtude = -64;
-    go_south_east(latitude, longtude);
+    go_south_east(&latitude, &longtude);
     printf("Avast: Now at: {%i, %i}\n", latitude, longtude);
+    printf("%i", latitude);
     return (0);
 }
