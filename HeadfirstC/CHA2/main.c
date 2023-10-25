4288a33ee4af4c54c0bc7cae8e9341fa7e71ab0e
khalid1sey <lilahseyfu@gmail.com>
2023-10-25 15:16:39 +0300

    done

--- HeadfirstC/CHA2/main.c
+++ HeadfirstC/CHA2/main.c
@@ -9,13 +9,14 @@ int main(void){
 
 
     char msg[] = "Hello World this is c";
-   
+    
     printf("size of msg  : %li\n", sizeof(msg)); //the characters in msg are actually 11 but there  null terminating at the last (\0) so they are 12
     
+    printf("size of msg  : %c\n", 3[msg]);
     printf("adress of msg  : %p\n", &msg);
     printf("adress of msg  0 : %p\n", &msg[0]); // the first adress and the msg variable adress is the same so it true
     printf("adress of msg  1 : %p\n", &msg[1]);
-    printf("adress of msg  5: %a\n", &msg[5]);
+    printf("adress of msg  5: %p\n", &msg[5]);
     printf("adress of msg  null terminating 12: %p\n", &msg[12]);
     return 0;
 }
